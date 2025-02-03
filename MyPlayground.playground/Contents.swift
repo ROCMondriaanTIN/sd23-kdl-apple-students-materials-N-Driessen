153c2a1f93842ee2fbcc956e011253b514522e4b
N-Driessen <302626668@student.rocmondriaan.nl>
2025-02-03 12:57:59 +0100

    les 2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,16 @@
-//Les 1 Apple Dev
+//Les 1
 import Foundation
 
 var greating = "Hello, playground!"
 
 print(greating)
+
+//Les 2
+var number1 = 3;
+var number2 = 6;
+
+var sum = number1 + number2;
+
+print("total: \(sum)")
+
+
